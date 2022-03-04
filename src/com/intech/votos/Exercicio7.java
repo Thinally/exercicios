cf290ee610724161b5402e3c89bca946e56070d5
thinally <thinallymagno@gmail.com>
2022-03-04 11:23:45 -0300

    Correcao exercicio 7

--- src/com/intech/votos/Exercicio7.java
+++ src/com/intech/votos/Exercicio7.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Exercicio7 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int num = 0;
+        int numero = 0;
         int par = 0;
 
-        num = sc.nextInt();
-        if(num % 2 == 0) {
-            par  = num + 5;
+        numero = sc.nextInt();
+        if(numero % 2 == 0) {
+            par  = numero + 5;
         }else {
-            par = num + 8;
+            par = numero + 8;
         }
         System.out.println(par);
     }
